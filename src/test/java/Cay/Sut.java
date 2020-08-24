3a057f8d2f67180559d4df96657abb3cdc8bfd75
aicetin <icetin35@gmail.com>
2020-08-24 11:39:04 -0500

    Sekersiz sut

---
+++ src/test/java/Cay/Sut.java
@@ -0,0 +1,9 @@
+package Cay;
+
+public class Sut {
+    public static void main(String[] args) {
+        System.out.println("Cok sekerli sut");
+
+        System.out.println("Sekersiz sut");
+    }
+}
