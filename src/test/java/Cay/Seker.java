f7c391d45ab8dc49a5889c24e88273b3af87a9ff
aicetin <icetin35@gmail.com>
2020-08-24 10:24:58 -0500

    cok seker

--- src/test/java/Cay/Seker.java
+++ src/test/java/Cay/Seker.java
@@ -2,7 +2,10 @@ package Cay;
 
 public class Seker {
     public static void main(String[] args) {
+
+
         System.out.println("Tek sekerli ceeeeek");
+        System.out.println("Cok sekerli ceeeeek");
 
     }
 }
